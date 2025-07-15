2fb8f8a7a530a668179b31284d28d0f89122758e
takintunde47 <akintund@purdue.edu>
2025-07-15 09:06:30 -0400

    adding fsmand synckey

---
+++ verilog/rtl/team_projects/team_06/src/team_06_FSM.sv
@@ -0,0 +1,95 @@
+module FSM(
+    input logic clk,
+    input logic rst,
+    input logic [7:0]mic_aud,       // Live audio being transmitted
+    input logic [7:0] spk_aud,      // Live audio being listened to
+    input logic ng_en,              // Noise gate enabale
+    input logic  ptt_en,            //Push-to-talk enable
+    input logic effect,             // Effect being used
+    input logic mute,               // Mute enabled
+    output logic [1:0]state,        // State we are currently in
+    output logic eff_en,            // Whether the current effect used will be enabled
+    output logic vol_en             // Whether volume is enabled or not
+);
+typedef enum logic [1:0] { 
+    LIST = 2'b00,
+    TALK = 2'b01
+ } state_t;
+
+logic[1:0] current_state, next_state;   // Variables for controlling the state case-satatements
+logic [7:0] threshold;                  // Threshold for which the mic_audio should pass in noise gate
+logic check;                // check logic for if we're above the threshold or not
+logic eff_en_temp;          // effect enable temporaryvariable
+logic spk_active;           // logic for if speaker is active
+
+assign threshold = 8'd64; // threshold is 64 decibels
+
+// Synchroning the state with the clock
+always_ff @(posedge clk, posedge rst) begin
+    if (rst)
+           current_state <= LIST;
+        else
+            current_state <= next_state;
+    end
+
+// Combinational: next state logic
+always_comb begin
+next_state = current_state;
+check = (mic_aud >= threshold);  // Is the mic signal above threshold?
+spk_active = (spk_aud != 0);    // speaker is active logic
+
+ /* Case statements for switching between states
+ based on the current state and certain conditions (MEALY)*/
+case (current_state)
+LIST: begin
+     if (spk_active) begin
+            next_state = LIST;
+     end
+     
+     else if ((ng_en && !ptt_en && check) || ptt_en) begin
+            next_state = TALK;
+     end 
+            end
+
+TALK: begin
+    if ((!ptt_en && !ng_en) || (ng_en && !ptt_en && !check)) begin
+             next_state = LIST;
+    end else if (spk_active) begin
+            next_state = LIST;
+    end
+            end
+
+default: next_state = LIST;
+endcase
+end
+assign state = current_state;
+
+    // Combinational: output logic
+    always_comb begin
+        vol_en = 0;
+        eff_en = 0;
+
+        case (current_state)
+            LIST: begin
+                if (!mute)
+                    vol_en = 1;
+                else
+                    vol_en = 0;
+                eff_en = 0;
+            end
+
+            TALK: begin
+                vol_en = 0;
+                if (effect)
+                    eff_en = 1;
+                else
+                    eff_en = 0;
+            end
+
+            default: begin
+                vol_en = 0;
+                eff_en = 0;
+            end
+        endcase
+    end
+endmodule
---
+++ verilog/rtl/team_projects/team_06/src/team_06_synckey.sv
@@ -0,0 +1,141 @@
+module synckey (
+    input  logic [3:0] pbs,    // 4 pushbuttons
+    input  logic clk,          // 25 mHz clock
+    input  logic rst,          // Active-high reset
+    input logic [1:0]vol,      // volume input for the volume knob
+    output logic [3:0] volume, // volume output logic for the volume shifter module
+    output logic ptt,          // push-to-talk mode enabled
+    output logic noise_gate,   // noise gate mode enabled
+    output logic effect,       // effects enabled
+    output logic mute          // mute button enabled
+);
+
+logic [3:0] prevPBS, currPBS;  // synchronization signals for push-buttons
+logic [1:0] prevV, currV;         // synchronization signals for volume knob
+logic [3:0] prev_syncPBS; // previous synchronized values for push-buttons for the edge detector
+logic [1:0] prev_syncV;   // previous synchronized values for volume knob for the edge detector
+logic [3:0] next_in;       // "next" signal logic for edge detetctor
+logic CW, ACW;          // clockwise and anti-clockwise logic
+logic [3:0] new_volume; // temporary variable for the volmue output
+logic mute_en;      // temporary variable for the mute button
+logic ng_en;        // temporary variable for the noise gate
+logic ptt_en;       // temporary variable for the push-to-talk
+logic effect_en;    // temporary variable for teh effect output
+
+typedef enum logic [1:0] {  
+    ENC1 = 2'b00,
+    ENC2 = 2'b01,
+    ENC3 = 2'b11,
+    ENC4 = 2'b10
+} ENC_t;
+logic[1:0] current_ENC, previous_ENC; // variables we need for comparing the encoded combinations
+
+
+// 2-stage synchronizer for pushbuttons and volume knob
+always_ff @(posedge clk, posedge rst) begin
+    if (rst) begin
+        prevPBS <= 4'b0;
+        currPBS <= 4'b0;
+        prevV <= 2'b0;
+        currV <= 2'b0;
+    end else begin
+        prevPBS <= pbs;
+        currPBS <= prevPBS;
+        prevV <= vol;
+        currV <= prevV;
+    end
+end
+
+// Memory for storing previous synchronized signal
+// for the push buttons, so we can compare the signals
+// to detect edge 
+always_ff @(posedge clk, posedge rst) begin
+    if (rst) begin
+        prev_syncPBS <= 4'b0;
+    end else begin
+        prev_syncPBS <= currPBS;
+    end
+end
+
+// Edge detector logic for the push buttons: mute, push-to-talk, effects, and mute
+always_comb begin
+mute_en = mute;
+effect_en = effect;
+ptt_en = ptt;
+ng_en = noise_gate;
+
+if (next_in[1] & ~prev_syncPBS[1]) begin // rising edge for mute
+            mute_en = ~mute_en;
+end else if (next_in[2] & ~prev_syncPBS[2]) begin // rising edge for effects
+            effect_en = ~effect_en;
+end else if (next_in[3] & ~prev_syncPBS[3]) begin// rising edge noise gate
+            ng_en = ~ng_en;
+end else if (next_in[0]) begin // rising edge for the push-to-talk button
+            ptt_en = 1;
+end
+end
+
+// flip-flop to update the pbs enable state store the output of the push buttons
+always_ff @(posedge clk, posedge rst) begin
+    if (rst) begin
+        mute <= 0;
+        effect <= 0;
+        noise_gate <= 0;
+        ptt <= 0;
+    end else begin
+        mute <= mute_en;
+        effect <= effect_en;
+        noise_gate <= ng_en;
+        ptt <= ptt_en;
+    end
+end
+
+assign current_ENC = currV;
+assign previous_ENC = prevV;
+
+// Encoder, monitoring the direction of the changing of each of these bits 
+always_comb begin
+    case({previous_ENC, current_ENC}) 
+        {ENC1, ENC2}, // 00 → 01
+        {ENC2, ENC3}, // 01 → 11
+        {ENC3, ENC4}, // 11 → 10
+        {ENC4, ENC1}: begin// 10 → 00
+        CW = 1;
+        ACW = 0;
+        end
+        {ENC1, ENC4}, // 00 → 10
+        {ENC4, ENC3}, // 10 → 11
+        {ENC3, ENC2}, // 11 → 01
+        {ENC2, ENC1}: begin   // 01 → 00
+        ACW = 1;
+        CW = 0;
+        end
+        default:  begin
+         CW = 0;
+         ACW = 0;
+        end
+    endcase
+end
+
+// Combinational logic for the increasing or reducing the volume
+always_comb begin
+new_volume = volume;
+if (CW && new_volume < 4'd15) begin
+   new_volume = new_volume + 1; 
+end
+else if(ACW && new_volume > 4'd0) begin
+    new_volume = new_volume - 1;
+end
+end
+
+// flip-flop for updating and storing the volume output of the module
+always_ff @(posedge clk, posedge rst) begin
+if (rst) begin
+volume <= 4'd0;
+end else begin
+volume <= new_volume;
+end
+
+end
+
+endmodule

994066401c6ff53f837f44bb67ea2ebd9d9039d0
Toluwani101 <toluwaniolasoko@gmail.com>
2025-07-15 10:39:38 -0400

    echo changed again

--- verilog/rtl/team_projects/team_06/src/team_06_echo_effect.sv
+++ verilog/rtl/team_projects/team_06/src/team_06_echo_effect.sv
@@ -4,22 +4,21 @@ module team_06_echo_effect (
   input logic [7:0] audio_in, //original audio entering echo module 
   input logic echo_enable, //when the echo module is enabled it becomes 1
   input logic [7:0] past_output, //past_output coming from memory
-  output logic [12:0] offset, //
+  output logic [12:0] offset, //amount of spaces back we go to get the past output
   output logic search, //searching for past output from memory
   output logic [7:0] echo_out, //the echo output
-  output logic [7:0] save_audio
+  output logic [7:0] save_audio //what is being sent to SRAM
 );
 
 //ECHO = (audio_in  + c*past_output)/(1 + C) 
-logic [7:0] current_out;
+logic [7:0] current_out; //temporary echo output 
 
-assign save_audio = audio_in;
-
-always_ff @(posedge clk or posedge rst) begin //sequential logic 
+assign save_audio = audio_in;//what is being sent to SRAM 
+always_ff @(posedge clk or posedge rst) begin 
   if(rst)begin
-    echo_out <= 0;  //for every reset, we don't want any echo output 
+    echo_out <= 0;  
   end else begin
-    echo_out <= current_out; // every other case except reset, current output becomes echo output
+    echo_out <= current_out; 
   end
 end
 
@@ -30,7 +29,7 @@ always_comb begin
     search = 1; //when echo_enable is on, we want to start searching the readwrite for past output from SRAM
     current_out = (audio_in + past_output) >> 1; //the echo formula: we are using C as 1, 
   end else begin
-    current_out = audio_in; //else, 
+    current_out = audio_in; 
     search = 0;
   end
 end

89ab621e74b974c170414a1eae6a68209550df5d
BenjaminSisk <siskb@purdue.edu>
2025-07-15 11:21:10 -0400

    small adjustments to adc

--- verilog/dv/team_06/module_tests/team_06_adc_to_i2s_tb.sv
+++ verilog/dv/team_06/module_tests/team_06_adc_to_i2s_tb.sv
@@ -3,6 +3,7 @@ module team_06_adc_to_i2s_tb;
     logic adc_serial_in; //adc sends msb first, so we shift right
     logic [7:0] spi_parallel_out;// spi_parallel_out will always be 0 unitl it collects all 8 bits
     logic finished;
+    logic [7:0] temp;
 
     team_06_adc_to_i2s DUT(.spiclk(spiclk), .rst(rst), .adc_serial_in(adc_serial_in), .spi_parallel_out(spi_parallel_out), .finished(finished));
 
@@ -10,7 +11,6 @@ module team_06_adc_to_i2s_tb;
         spiclk = 0;
         forever #10 spiclk = ~spiclk;
     end
-
     initial begin
         $dumpfile("team_06_adc_to_i2s.vcd");
         $dumpvars(0, team_06_adc_to_i2s_tb);
@@ -33,7 +33,7 @@ module team_06_adc_to_i2s_tb;
         rst = 0;
 
         //another way by using for loop. we have to send msb first
-        logic [7:0] temp = 8'b11010110;
+        temp = 8'b11010110;
         for(int i = 7; i >= 0; i--) begin
             adc_serial_in = temp[i]; @(posedge spiclk);
         end
--- verilog/rtl/team_projects/team_00/team_00_WB.v
+++ verilog/rtl/team_projects/team_00/team_00_WB.v
@@ -25,40 +25,66 @@
 `define				WB_AW		16
 
 
-module team_06_WB (
+module team_00_WB (
 `ifdef USE_POWER_PINS
 	inout vccd1,    // User area 1 1.8V supply
 	inout vssd1,    // User area 1 digital ground
 `endif
 	`WB_SLAVE_PORTS,
+	input	wire	[32-1:0]	la_data_in,
+	output	wire	[32-1:0]	la_data_out,
+	input	wire	[32-1:0]	la_oenb,
 	input	wire	[34-1:0]	gpio_in,
 	output	wire	[34-1:0]	gpio_out,
 	output wire [34-1:0] gpio_oeb,
-input wire nrst,
-	input	wire	[32-1:0]	DAT_I,
-	input	wire	[1-1:0]	ACK_I,
-	output	wire	[32-1:0]	ADR_O,
-	output	wire	[32-1:0]	DAT_O,
-	output	wire	[4-1:0]	SEL_O,
-	output	wire	[1-1:0]	WE_O,
-	output	wire	[1-1:0]	STB_O,
-	output	wire	[1-1:0]	CYC_O
+input wire nrst
 );
 
 	localparam	EN_VAL_REG_OFFSET = `WB_AW'h0000;
+	localparam	PRESCALER_VAL_REG_OFFSET = `WB_AW'h0004;
+	localparam	IM_REG_OFFSET = `WB_AW'hFF00;
+	localparam	MIS_REG_OFFSET = `WB_AW'hFF04;
+	localparam	RIS_REG_OFFSET = `WB_AW'hFF08;
+	localparam	IC_REG_OFFSET = `WB_AW'hFF0C;
 	wire		clk = clk_i;
 
 
 	`WB_CTRL_SIGNALS
 
 	wire [1-1:0]	en;
+	wire [14-1:0]	prescaler;
+	wire [1-1:0]	done;
 
 	// Register Definitions
 	reg [0:0]	EN_VAL_REG;
 	assign	en = EN_VAL_REG;
 	`WB_REG(EN_VAL_REG, 0, 1)
 
-	team_06 instance_to_wrap (
+	reg [13:0]	PRESCALER_VAL_REG;
+	assign	prescaler = PRESCALER_VAL_REG;
+	`WB_REG(PRESCALER_VAL_REG, 0, 14)
+
+	reg [0:0] IM_REG;
+	reg [0:0] IC_REG;
+	reg [0:0] RIS_REG;
+
+	`WB_MIS_REG(1)
+	`WB_REG(IM_REG, 0, 1)
+	`WB_IC_REG(1)
+
+	wire [0:0] DONE_ASSERT = done;
+
+
+	integer _i_;
+	`WB_BLOCK(RIS_REG, 0) else begin
+		for(_i_ = 0; _i_ < 1; _i_ = _i_ + 1) begin
+			if(IC_REG[_i_]) RIS_REG[_i_] <= 1'b0; else if(DONE_ASSERT[_i_ - 0] == 1'b1) RIS_REG[_i_] <= 1'b1;
+		end
+	end
+
+	assign IRQ = |MIS_REG;
+
+	team_00 instance_to_wrap (
 	`ifdef USE_POWER_PINS
 		.vccd1(vccd1),    // User area 1 1.8V power
 		.vssd1(vssd1),    // User area 1 digital ground
@@ -66,21 +92,23 @@ input wire nrst,
 		.clk(clk),
 		.nrst(nrst),
 		.en(en),
+		.prescaler(prescaler),
+		.done(done),
+		.la_data_in(la_data_in),
+		.la_data_out(la_data_out),
+		.la_oenb(la_oenb),
 		.gpio_in(gpio_in),
 		.gpio_out(gpio_out),
-		.gpio_oeb(gpio_oeb),
-		.DAT_I(DAT_I),
-		.ACK_I(ACK_I),
-		.ADR_O(ADR_O),
-		.DAT_O(DAT_O),
-		.SEL_O(SEL_O),
-		.WE_O(WE_O),
-		.STB_O(STB_O),
-		.CYC_O(CYC_O)
+		.gpio_oeb(gpio_oeb)
 	);
 
 	assign	dat_o = 
 			(adr_i[`WB_AW-1:0] == EN_VAL_REG_OFFSET)	? EN_VAL_REG :
+			(adr_i[`WB_AW-1:0] == PRESCALER_VAL_REG_OFFSET)	? PRESCALER_VAL_REG :
+			(adr_i[`WB_AW-1:0] == IM_REG_OFFSET)	? IM_REG :
+			(adr_i[`WB_AW-1:0] == MIS_REG_OFFSET)	? MIS_REG :
+			(adr_i[`WB_AW-1:0] == RIS_REG_OFFSET)	? RIS_REG :
+			(adr_i[`WB_AW-1:0] == IC_REG_OFFSET)	? IC_REG :
 			32'hDEADBEEF;
 
 	always @ (posedge clk_i or posedge rst_i)

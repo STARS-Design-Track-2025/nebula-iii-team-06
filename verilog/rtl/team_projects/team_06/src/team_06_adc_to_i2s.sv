f10c3e3f00c528d56adf7e9ed5947322d4f5ad00
BenjaminSisk <siskb@purdue.edu>
2025-07-15 11:50:47 -0400

    Finalized ADC to i2s

--- verilog/rtl/team_projects/team_06/src/team_06_adc_to_i2s.sv
+++ verilog/rtl/team_projects/team_06/src/team_06_adc_to_i2s.sv
@@ -6,8 +6,11 @@ module team_06_adc_to_i2s
     output logic finished // this is to know if our 8 bit register recieve 8bbits form ADC
 
 );
-    logic [3:0] counter; // counter is used to count how many bits we have right now. it will count from 1 to 8
-    logic [7:0] out_temp;
+    logic [2:0] counter, counter_n; // counter is used to count how many bits we have right now. it will count from 1 to 8
+    logic [7:0] out_temp, out_temp_n, spi, spi_parallel_out_n;
+    logic finished_n;
+
+
     always_ff @(posedge spiclk or posedge rst) begin
         if(rst) begin
             counter <= '0;
@@ -15,11 +18,18 @@ module team_06_adc_to_i2s
             finished <= '0;
             spi_parallel_out <= '0;
         end else begin
-            counter <= (counter == 4'd8)? 0: counter +1;
-            out_temp <= {out_temp[6:0], adc_serial_in};
-            finished <= (counter == 4'd8)? 1:0;                                                   
-            spi_parallel_out <= (counter == 4'd8)? {out_temp[6:0], adc_serial_in} : 0;
+            counter <= counter_n;
+            out_temp <= out_temp_n;
+            finished <= finished_n;                                                  
+            spi_parallel_out <= spi_parallel_out_n; 
         end
     end
+
+    always_comb begin
+        counter_n = counter + 1;
+        out_temp_n = {out_temp[6:0], adc_serial_in};
+        finished_n = (counter == 3'd7); 
+        spi_parallel_out_n = (counter == 3'd7) ? {out_temp[6:0], adc_serial_in} : spi_parallel_out;
+    end 
 
 endmodule

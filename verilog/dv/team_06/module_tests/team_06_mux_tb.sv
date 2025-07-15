bd5b2077659006c28ccdfc13776eca0122a7e3c4
takintunde47 <akintund@purdue.edu>
2025-07-15 16:37:30 -0400

    mux module

--- verilog/dv/team_06/module_tests/team_06_mux_tb.sv
+++ verilog/dv/team_06/module_tests/team_06_mux_tb.sv
@@ -5,7 +5,7 @@ module team_06_echo_effect_tb;
 logic [7:0] dry_audio;
 logic [7:0] final_audio;
 logic [1:0] selector;
-logic [7:0] echo_out;
+logic [7:0] echo_output;
 logic [7:0] soft_out;
 
 
@@ -14,7 +14,7 @@ logic [7:0] soft_out;
 // Instantiation of the echo module
 team_06_mux sahur (
 .selector(selector),
-.echo_out(echo_out),
+.echo_out(echo_output),
 .soft_out(soft_out),
 .dry_audio(dry_audio),
 .final_audio(final_audio)
@@ -29,32 +29,32 @@ initial begin
 
 
 
-echo_out = 0;
+echo_output = 0;
 soft_out = 0;
 dry_audio = 0;
 dry_audio = 0;
 
 
 #1
-echo_out = 1;
+echo_output = 1;
 soft_out = 1;
 dry_audio =1;
 dry_audio = 1;
 
 #1
-echo_out = 0;
+echo_output = 0;
 soft_out = 0;
 dry_audio = 0;
 dry_audio = 0;
 
 #1
-echo_out = 1;
+echo_output = 1;
 soft_out = 1;
 dry_audio =1;
 dry_audio = 1;
 
 #1
-echo_out = 0;
+echo_output = 0;
 soft_out = 0;
 dry_audio = 0;
 dry_audio = 0;

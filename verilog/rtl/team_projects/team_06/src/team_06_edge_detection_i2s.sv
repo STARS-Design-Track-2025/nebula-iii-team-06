2e5dafba0bb752ab4801721b3c20f2107b716f8c
tdlam2310 <ldt2310@gmail.com>
2025-07-16 11:28:57 -0400

    fixed testbench

--- verilog/rtl/team_projects/team_06/src/team_06_edge_detection_i2s.sv
+++ verilog/rtl/team_projects/team_06/src/team_06_edge_detection_i2s.sv
@@ -1,14 +1,14 @@
 module team_06_edge_detection_i2s(
     input logic i2sclk, clk, rst,
-    output logic curr_i2sclk, past_i2sclk
+    output logic past_i2sclk
 );
     always_ff @(posedge clk or posedge rst) begin
         if (rst) begin
-            curr_i2sclk <= 0;
+            //curr_i2sclk <= 0;
             past_i2sclk <= 0;
         end else begin
-            past_i2sclk <= curr_i2sclk;
-            curr_i2sclk <= i2sclk;
+            past_i2sclk <= i2sclk;
+             //curr_i2sclk <= i2sclk;
         end
-end
+    end
 endmodule
